b4c6a47d7fa6cfc63f66eb3de7d80121b17fb57d
Asim Okby <34512671+asimokby@users.noreply.github.com>
2019-10-16 22:30:46 +0300

    Update solution.py

--- Valid_Palindrome/solution.py
+++ Valid_Palindrome/solution.py
@@ -1,3 +1,15 @@
+#Algoithm: 
+#     => create two pointers i and j where i will point to the first element in the string,
+#        and j will point to the last one.
+#     => scan from right and left, if both elements at i and j are alphanumeric chars,
+#        and equal to each other, increment i and decrement j. 
+#     => if any of the elements is not alphanumeric, increment i if it is right, decrement j if it is left,
+#        and continue (without checking equality) scanning till both chars are alphanumeric. 
+#     => if both chars are alphanumeric and are not equal, return false. 
+#     => if i == j or j < i, which means that the two pointers met without finding any unequal chars, return true.
+    
+
+
 class Solution(object):
     def isPalindrome(self, s):
         i = 0 
